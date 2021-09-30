11cc1809dec18c7852dff1f22b3bec7c3d84dbc2
Sarah A. Abu Zeneh <sara.abuzena2000@gmail.com>
2021-09-30 17:32:01 +0300

    Sarah and Sondos H.W Solution

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
 const express = require("express");
 
 const app = express();
-const arr=[{
-
-    name : "Sarah",
-    age : 20,
-},
-{
-    name : "Omar",
-    age: 20,
-}
+const arr = [
+    {
+        name: "Sarah",
+        age: 20
+    }, {
+        name: "Omar",
+        age: 20
+    }
 ];
-app.get("/",  (req, res) => {
+app.get("/", (req, res) => {
     res.send("First Try");
 });
-app.get("/users" , (req , res)=>{
+app.get("/users", (req, res) => {
     res.json(arr);
 })
 
-app.get("/search" , (req , res)=>{
-    const {name, age}=req.query;
-    const filterdArray= arr.filter(user=>{
-        if(name!=undefined && age!=undefined){
-           return ( user.name.includes(name)&&
-            user.age==age)
-        }else if(name!=undefined){
-            return (user.name.includes(name)&&user.age("Age"))
+app.get("/search", (req, res) => {
+    const {name, age} = req.query;
+    let filterdArray = arr;
 
-        }else if(age!=undefined){
-            return (user.age(age)&&user.name.includes("User Name"))
+    if (name) {
+        filterdArray = arr.filter(user => user.name.includes(name))
+    }
+    if (color) {
+        filterdArray = arr.filter(user => user.age = age)
+    }
 
-        }else if (filterdArray.length==0){
-            return (res.statusCode)
-        }
-    })
-    res.json(filterdArray);
+    if (filterdArray.length === 0) {
+        res.status(204).send('No Content -> Empty Array')
+    } else if (filterdArray.length != 0) {
+        res.status(200).json(filterdArray)
+    }
 })
-app.listen(3000,console.log("it's Worked"));
+app.listen(3000, console.log("it's Worked"));
